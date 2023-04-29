f1d1b2ff12db2713f930d72c57300994b77b347a
Md Asjad <mdasjad895@gmail.com>
2023-04-29 19:57:26 +0530

    updated

--- Famlib/Famlib/settings.py
+++ Famlib/Famlib/settings.py
@@ -119,5 +119,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mdasjad895@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
