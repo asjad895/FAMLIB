a3e3646e4dfd2fd10ca198623e466541f1e179ec
Md Asjad <mdasjad895@gmail.com>
2023-04-29 19:23:02 +0530

    updated

--- Famlib/Famlib/settings.py
+++ Famlib/Famlib/settings.py
@@ -120,4 +120,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'mdasjad895@gmail.com'
-EMAIL_HOST_PASSWORD = 'xeadfbneggeoclyl'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
